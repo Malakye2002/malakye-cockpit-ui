3fa652ab21e8f369ad897dbfed0ad80ffaef607e
MALAKYE-2002 <155997043+MALAKYE-2002@users.noreply.github.com>
2025-11-02 08:56:43 -0800

    Update fetch URL to use VERCEL_URL environment variable

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/github`, {
+  const res = await fetch(`${process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : ""}/api/github`, {
     cache: "no-store",
   });
   const data = await res.json();
